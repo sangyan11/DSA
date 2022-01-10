477925a73d9aaf59d78600a9b95961f7d31b0cc4
sangyan11 <Sangyan@11>
2022-01-10 14:14:13 +0530

    added questions

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,16 @@
 package com.company;
 
-import Stack.MyStack;
+import LinkedList.MyLinkedList;
+import LinkedList.Stack;
 
 public class Main {
 
 
 
     public static void main(String[] args) {
+     int arr [] = {3,2,5,1,4,8,9};
+     int sum =0;
+
+
     }
 }
